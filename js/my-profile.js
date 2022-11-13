cf5965efce7ad45b2024954e570215236060bfdc
romicaubarrere <rominacaubarrere@gmail.com>
2022-11-13 14:39:08 -0300

    entrega 7

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,37 +1,3 @@
-function showUserData() {
-  //escribe el usuario guardado en el login (localstorage)
-  // console.log(localStorage.getItem("email")); //diferencia entre comillas y sin comillas
-  // console.log(localStorage.getItem(email));
-  document.getElementById("usuario").innerHTML = localStorage.getItem("email");
-  document.getElementById("imagen").value = localStorage.getItem("img");
-  document.getElementById("name1").value = localStorage.getItem("name");
-  document.getElementById("name2").value = localStorage.getItem("secondName");
-  document.getElementById("email1").value = localStorage.getItem("mail");
-  document.getElementById("contact1").value = localStorage.getItem("contacto");
-  document.getElementById("surname1").value = localStorage.getItem("surname");
-  document.getElementById("surname2").value = localStorage.getItem("surname2");
-}
-//
-//funcion para tocar cambiar datos agregar info
-//
-function activar() {
-  console.log("activando");
-  let des = document.getElementsByTagName("input");
-
-  for (desconectados of des) {
-    desconectados.removeAttribute("disabled");
-  }
-}
-
-function desactivar() {
-  console.log("activando");
-  let des = document.getElementsByTagName("input");
-
-  for (desconectados of des) {
-    desconectados.setAttribute("disabled", "false");
-  }
-}
-
 //
 //funcion desafiate para que se pueda cambiar la foto
 //
@@ -51,48 +17,67 @@ function foto() {
   }
 }
 
+function showUserData() {
+  //escribe el usuario guardado en el login (localstorage)
+  console.log(localStorage.getItem("email")); //diferencia entre comillas y sin comillas
+  document.getElementById("usuario").innerHTML = localStorage.getItem("email");
+  document.getElementById("imagen").value = localStorage.getItem("img");
+  document.getElementById("primerNombre").value =
+    localStorage.getItem("primerNombre");
+  document.getElementById("segundoNombre").value =
+    localStorage.getItem("segundoNombre");
+  document.getElementById("correo").value = localStorage.getItem("correo");
+  document.getElementById("telefono").value = localStorage.getItem("telefono");
+  document.getElementById("primerApellido").value =
+    localStorage.getItem("primerApellido");
+  document.getElementById("segundoApellido").value =
+    localStorage.getItem("segundoApellido");
+}
+
+(function () {
+  "use strict";
+  var forms = document.querySelectorAll(".needs-validation");
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        } else {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
 function guardarCambios() {
-  //
-  document.querySelectorAll("input").forEach((e) => e.reportValidity());
-
   const img = document.getElementById("imagen").src;
-  const name = document.getElementById("name1").value;
-  const secondName = document.getElementById("name2").value;
-  const surname = document.getElementById("surname1").value;
-  const surname2 = document.getElementById("surname2").value;
-  const mail = document.getElementById("email1").value;
-  const contacto = document.getElementById("contact1").value;
+  const name = document.getElementById("primerNombre").value;
+  const secondName = document.getElementById("segundoNombre").value;
+  const surname = document.getElementById("primerApellido").value;
+  const surname2 = document.getElementById("segundoApellido").value;
+  const mail = document.getElementById("correo").value;
+  const contacto = document.getElementById("telefono").value;
 
-  localStorage.setItem("img", img);
-  localStorage.setItem("name", name);
-  localStorage.setItem("secondName", secondName);
-  localStorage.setItem("surname1", surname);
-  localStorage.setItem("surname2", surname2);
-  localStorage.setItem("mail", mail);
-  localStorage.setItem("contacto", contacto);
+  localStorage.setItem("imagen", img);
+  localStorage.setItem("primerNombre", name);
+  localStorage.setItem("segundoNombre", secondName);
+  localStorage.setItem("primerApellido", surname);
+  localStorage.setItem("segundoApellido", surname2);
+  localStorage.setItem("correo", mail);
+  localStorage.setItem("telefono", contacto);
+}
 
-  desactivar();
+function eliminarCambios() {
+  localStorage.clear();
+  location.href = "login.html";
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   showUserData(); //cuando carga la pagina enseguida me muestra el usuario precargado
-  //
-  //funcion que guarda los cambios hechos
-  //
-
-  document.getElementById("save").addEventListener("click", () => {
-    guardarCambios();
-  });
-  document.getElementById("change").addEventListener("click", () => {
-    activar();
-  });
-
-  let inputs = document.getElementsByTagName("input");
-  console.log(inputs);
-  document.getElementById("save").addEventListener("onclick", () => {
-    inputs.forEach((input) => {
-      console.log("inputs");
-      input.checkValidity();
-    });
-  });
 });
